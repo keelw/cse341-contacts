b7b7208672361eea1c70e0f78b3a8fe4bc28d8e7
keelw <wjordankeel@gmail.com>
2024-03-04 17:13:24 -0700

    switch from users to contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,18 +3,18 @@ const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async(req, res) => {
     const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
+    result.toArray().then((contacts) => {
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json(users);
+        res.status(200).json(contacts);
     });
 }
 
 const getSingle = async(req, res) => {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
-    result.toArray().then((users) => {
+    result.toArray().then((contacts) => {
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json(users[0]);
+        res.status(200).json(contacts[0]);
     });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ router.get("/", (req, res) => {
     res.send('Hello World')
 });
 
-router.use("/users", require("./users"));
+router.use("/contacts", require("./contacts"));
 
 module.exports = router;
