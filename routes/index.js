2610d754534e614407c022c99ee1501e3279e686
keelw <wjordankeel@gmail.com>
2024-03-08 23:38:21 -0700

    added post, get, put, delete, and installed/implemented swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
+  //#swagger.tags=['Hello World']
   res.send('Hello World');
 });
 
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description: 'Contacts API'
+  },
+  host: 'localhost:3000',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
